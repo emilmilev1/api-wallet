132bc0dd1eae3562497bc9d1e9dc6c516c8857c1
Emil Milev <emo.2012012@gmail.com>
2024-12-07 22:08:32 +0200

    Update data of user in database

--- src/database/dbInit.ts
+++ src/database/dbInit.ts
@@ -1,5 +1,5 @@
-import { logger } from "../utils/logger";
-import dbClient from "./dbClient";
+import { logger } from '../utils/logger/logger';
+import dbClient from './dbClient';
 
 /**
  * @description Initializes the database connection
@@ -8,30 +8,30 @@ import dbClient from "./dbClient";
 export const dbInit = async (): Promise<void> => {
     try {
         await dbClient.$connect();
-        logger.info("Database connected successfully.");
+        logger.info('Database connected successfully.');
     } catch (error) {
-        logger.error("Database connection error: " + error);
+        logger.error('Database connection error: ' + error);
         process.exit(1);
     }
 
-    dbClient.$on("error", (error: any) => {
-        logger.error("Database error:", error);
+    dbClient.$on('error', (error: any) => {
+        logger.error('Database error:', error);
     });
 
     const gracefulShutdown = async (): Promise<never> => {
-        logger.warn("Shutting down. Closing database connection...");
+        logger.warn('Shutting down. Closing database connection...');
 
         try {
             await dbClient.$disconnect();
 
-            logger.warn("Database connection closed.");
+            logger.warn('Database connection closed.');
             process.exit(0);
         } catch (error) {
-            logger.error("Error disconnecting from database:", error);
+            logger.error('Error disconnecting from database:', error);
             process.exit(1);
         }
     };
 
-    process.on("SIGINT", gracefulShutdown);
-    process.on("SIGTERM", gracefulShutdown);
+    process.on('SIGINT', gracefulShutdown);
+    process.on('SIGTERM', gracefulShutdown);
 };
